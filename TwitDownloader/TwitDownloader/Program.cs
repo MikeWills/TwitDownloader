064463063994e25ba5897a729935e39c14ce9c74
Mike Wills <@MikeWills>
2016-11-21 10:57:37 -0600

    Basic communication is working

--- TwitDownloader/TwitDownloader/Program.cs
+++ TwitDownloader/TwitDownloader/Program.cs
@@ -15,30 +15,39 @@ namespace TwitDownloader
         static void Main(string[] args)
         {
             string url = String.Empty;
-            string appid = String.Empty;
-            string appkey = String.Empty;
+            string appid = "f6924e79";
+            string appkey = "05cb3378be72373bf0b3d5607c596b44";
             string showId = String.Empty;
+
+            // Make sure there is at least a show id included
+            if (args.Count() == 0)
+            {
+                Console.WriteLine("Missing required arguments.");
+                return;
+            }
 
             Arguments a = new Arguments(args);
 
-            if (!String.IsNullOrEmpty(a["s"]))
+            // Make sure -s is in there
+            if (String.IsNullOrEmpty(a["s"]))
             {
                 Console.WriteLine("Missing Show Id. Please include the parameter '-s <showid>'");
                 return;
             }
 
+            // Get the showid
             showId = a["s"];
-            showId = "1636";
 
-            using (HttpClientHandler handler = new HttpClientHandler { AllowAutoRedirect = true, UseCookies = true, CookieContainer = new CookieContainer() })
+            using (var client = new HttpClient())
             {
-                using (var client = new HttpClient(handler))
-                {
-                    url = String.Format("http://twit.tv/api/v1.0/episodes?filter%5Bshows%5D={0}", showId);
-                    var getResponse = client.GetAsync(url).Result;
-                    string seeResponse = getResponse.Content.ReadAsStringAsync().Result; // For data validation
-                    dynamic jsonResponse = JsonConvert.DeserializeObject<dynamic>(getResponse.Content.ReadAsStringAsync().Result);
-                }
+                client.DefaultRequestHeaders.Add("app-id", appid);
+                client.DefaultRequestHeaders.Add("app-key", appkey);
+                url = String.Format("http://twit.tv/api/v1.0/episodes?filter%5Bshows%5D={0}", showId);
+                var getResponse = client.GetAsync(url).Result;
+                string seeResponse = getResponse.Content.ReadAsStringAsync().Result; // For data validation
+                dynamic jsonResponse = JsonConvert.DeserializeObject<dynamic>(getResponse.Content.ReadAsStringAsync().Result);
+
+
             }
         }
     }
